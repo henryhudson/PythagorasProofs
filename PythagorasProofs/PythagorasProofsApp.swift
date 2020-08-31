4b5e368ae439f8dc4399372afb8ec35bc64068a2
Henry Hudson <henryhudson1431@yahoo.com>
2020-08-31 17:38:48 +0100

    Initial Commit

---
+++ PythagorasProofs/PythagorasProofsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PythagorasProofsApp.swift
+//  PythagorasProofs
+//
+//  Created by Henry Hudson on 31/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct PythagorasProofsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PythagorasProofsTests/PythagorasProofsTests.swift
@@ -0,0 +1,33 @@
+//
+//  PythagorasProofsTests.swift
+//  PythagorasProofsTests
+//
+//  Created by Henry Hudson on 31/08/2020.
+//
+
+import XCTest
+@testable import PythagorasProofs
+
+class PythagorasProofsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
